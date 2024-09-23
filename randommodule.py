1f78517f50adc02074d76cc49797a00f52124644
VaibhavUpendra <nuclearweapon211@gmail.com>
2024-09-23 15:38:04 +0530

    second commit

---
+++ randommodule.py
@@ -0,0 +1,13 @@
+import random
+
+rand_var = random.random()
+rand_int = random.randint(7,90)
+rand_rang = random.randrange(230,2350,5)
+unirand_var = random.uniform(3,5)
+
+sm_list = [2,3,42,65,4,8]
+ch_var = random.choice(sm_list)
+random.shuffle(sm_list)
+
+print(sm_list)
+
