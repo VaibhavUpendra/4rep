6c732c4e5559c3d01f53c1a0d35a5dcfbf20c592
VaibhavUpendra <nuclearweapon211@gmail.com>
2024-09-25 15:51:58 +0530

    fifth commit

---
+++ DateTimeModule.py
@@ -0,0 +1,34 @@
+#from datetime import datetime
+#dt = datetime(2024,9,25,9,25,00)
+#now = datetime.now()
+#print(now.year)
+
+#from datetime import date
+#d= date(2024,9,25)
+#print(d)
+
+#today =date.today()
+#print(today.year)
+
+#from datetime import time
+#t =  time(1,28,45)
+#print(t)
+#print(t.hour)
+
+#from datetime import timedelta,date
+#delta = timedelta(days=5,hours=3,minutes=30)
+#print("Timedelta: ",delta)
+#today = date.today()
+#print("date Before: ",today)
+
+#future_date = today +timedelta(days=7)
+#print("After Date: ",future_date)
+
+
+#from  datetime import datetime
+#date1 = datetime(2023,9,22,10,30)
+#date2 = datetime(2024,9,22,10,30)
+#print(date2-date1)
+
+from calendar import isleap
+print(isleap(2024))
